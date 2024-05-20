d8f52bf7a18a6d13c4ccb7d8de02155635003557
MrCra <mrcrabs67forms@outlook.com>
2024-05-20 20:01:49 +0300

    new project

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// import React from 'react';
+import './scss/app.scss';
+import {Header} from "./components/Header";
+import {Categories} from "./components/Categories";
+import {Sort} from "./components/Sort";
+import {PizzaBlock} from "./components/PizzaBlock";
+
+function App() {
+  return (
+      <div className="wrapper">
+          <div className="box1">
+              <div className="box2">SASS</div>
+          </div>
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock title="Чизбургер-пицца" price="395"/>
+            </div>
+          </div>
+        </div>
+      </div>
+)
+  ;
+}
+
+export default App;
