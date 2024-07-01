b305552609768ed1af312050e67ed9c1598a4178
MrCra <mrcrabs67forms@outlook.com>
2024-07-01 20:39:30 +0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,27 @@ import {Header} from "./components/Header";
 import {Categories} from "./components/Categories";
 import {Sort} from "./components/Sort";
 import {PizzaBlock} from "./components/PizzaBlock";
-import pizzas from "./assets/pizza.json"
+import {useEffect, useState} from "react";
+import {a} from "vite/dist/node/types.d-aGj9QkWt";
+import any = jasmine.any;
 
 function App() {
+
+
+    useEffect(() => {
+        fetch('https://668276584102471fa4c70a9d.mockapi.io/title')
+            .then((res) => res.json())
+            .then((arr) => {
+                setItems(arr);
+                console.log(items);
+            });
+        return () => {
+            // код при размонтировании компонента
+        };
+    }, []);
+    const [items, setItems] = useState(Array<any>);
     // console.log(pizzas);
+    //https://6655ebb93c1d3b60293b99b0.mockapi.io/marketPizza/
   return (
       <div className="wrapper">
         <Header />
@@ -20,7 +37,7 @@ function App() {
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
                 {
-                    pizzas.map((object) => <PizzaBlock {...object} key = {object.id} />)
+                    items.map((object) => <PizzaBlock {...object} key = {object.id} />)
                 }
             </div>
           </div>
